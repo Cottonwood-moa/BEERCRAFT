fadee8efa32e73b40afb1a11ec7cac9a5d8c7cc0
Cottonwood <blog_geon@naver.com>
2021-09-09 22:27:48 +0900

    no message

--- imageTrans/views.py
+++ imageTrans/views.py
@@ -15,7 +15,6 @@ import time
 import functools
 import test as t
 
-print(t.test(1,2))
 
 # def tensor_to_image(tensor):
 #   tensor = tensor*255
@@ -45,6 +44,7 @@ print(t.test(1,2))
 
 # Create your views here.
 def index(request):
+  testVar = t.test(1,2)
   if request.method == 'POST':
     # content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
     # style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
