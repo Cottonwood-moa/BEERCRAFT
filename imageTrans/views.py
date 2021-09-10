0171fb1294e7ce6ef81c32136b9b59c364b28c6b
Cottonwood <blog_geon@naver.com>
2021-09-10 16:02:06 +0900

    no message

--- imageTrans/views.py
+++ imageTrans/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # =================================
-# import tensorflow as tf
+import tensorflow as tf
 # import tensorflow_hub as hub
 # =================================
 import IPython.display as display
