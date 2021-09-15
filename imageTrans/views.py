907f86ed8e3b18fc409957d3840178db74b28cdf
Cottonwood <blog_geon@naver.com>
2021-09-15 23:55:29 +0900

    no message

--- imageTrans/views.py
+++ imageTrans/views.py
@@ -25,7 +25,7 @@ def tensor_to_image(tensor):
   return transedImg
 
 def load_img(path_to_img):
-  max_dim = 512dd
+  max_dim = 512
   img = tf.io.read_file(path_to_img)
   img = tf.image.decode_image(img, channels=3)
   img = tf.image.convert_image_dtype(img, tf.float32)
